85d7d9089ae509d42e7301c4ed9109a390348c77
danie <danielestudis@gmail.com>
2019-02-10 13:38:01 +0100

    Ejercicio Boxing and Unboxing.

---
+++ Boxing and Unboxing/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Boxing_and_Unboxing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //boxing
+            int valor = 49867;
+                        
+            object boxing = valor;
+
+            valor = 25;
+
+            Console.WriteLine("BOXING");
+            Console.WriteLine("-----------");
+            Console.WriteLine("El valor de la variable valor es {0}", valor);
+            Console.WriteLine("El valor de la variable boxing es {0}", boxing);
+            Console.Read();
+
+            //unboxing
+            object unboxing = 1234;
+
+            int ub = (int)unboxing;
+
+            Console.WriteLine("UNBOXING");
+            Console.WriteLine("----------");
+            Console.WriteLine("El valor de la variable unboxing es {0}", unboxing);
+            Console.WriteLine("El valor de la variable ub es {0}", ub);
+            Console.Read();
+
+        
+        }
+    }
+}
---
+++ Readme.md
@@ -0,0 +1,7 @@
+<h1>Boxing and Unboxing </h1>
+
+En este ejercicio vamos a enseñar que es el boxing y el unboxing.
+
+El boxing hace que el compilador este forzado a convertir un tipo de valor por un tipo de referencia para asi almacenarlo en el heap en vez de en el stack. Y el unboxing trata de hacer la inversa.
+
+Para saber cómo realizarlo, hemos generado un codigo el cual generará una variable de tipo int i la convertirá en objeto. Después cambiaremos su valor y veremos que variables son modificadas. Para el unboxing haremos lo mismo.
